f6e927d69e884385879ca71b0ad5461021946328
Taiyo <taiyoperson@gmail.com>
2025-01-24 22:47:14 +1300

    fix: Library.remove() not removing

--- script.js
+++ script.js
@@ -7,26 +7,34 @@ function Book(author, title, pages, read) {
 }
 
 function Library(name) {
-    this.name = name;
+    this.name = name,
     this.books = []
 }
 Library.prototype.add = function(author, title, pages, read) {
-    books.push(new Book(author, title, pages, read));
+    this.books.push(new Book(author, title, pages, read));
 }
 Library.prototype.remove = function(title) {
-    library.filter((book) => {
+    this.books = this.books.filter((book) => {
         return book.title !== title;
     }); 
 }
 Library.prototype.removeAll = function() {
-    books = [];
+    this.books = [];
 },
 Library.prototype.update = function(title, targetAttribute, newValue) {
-    books.map((book) => {
+    this.books.map((book) => {
         if (book.title === title) book[targetAttribute] = newValue;
     });
 }
 
+let myLibrary = new Library("My Library");
+
+myLibrary.add("Isaac Walterson", "Steve Jobs", "650", false);
+myLibrary.add("George Orwell", "1984", "540", true);
+myLibrary.add("Unknown", "Hitchhiker's Guide to the Universe", "850", false);
+myLibrary.remove("1984");
+myLibrary.update("Steve Jobs", "read", true);
+console.table(myLibrary.books);
 
 // DOM manipulation
 // 1. Create function to add new book to DOM
