10ecaae3d31cff059f2fe4914b32591686df1832
Taiyo <taiyoperson@gmail.com>
2025-01-25 01:38:12 +1300

    feat: Create card templates on button

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+let addButton = document.querySelector("#addButton");
+let removeAllButton = document.querySelector("#removeAllButton");
+let cardTemplate = document.querySelector("#cardTemplate");
+let article = document.querySelector("article");
+
 // Data logic 
-function Book(author, title, pages, read) {
-    this.author = author;
+function Book(title, author, pages, read) {
     this.title = title;
+    this.author = author;
     this.pages = pages;
     this.read = read;
 }
@@ -10,8 +15,8 @@ function Library(name) {
     this.name = name,
     this.books = []
 }
-Library.prototype.add = function(author, title, pages, read) {
-    this.books.push(new Book(author, title, pages, read));
+Library.prototype.add = function(title, author, pages, read) {
+    this.books.push(new Book(title, author, pages, read));
 }
 Library.prototype.remove = function(title) {
     this.books = this.books.filter((book) => {
@@ -34,11 +39,37 @@ myLibrary.add("George Orwell", "1984", "540", true);
 myLibrary.add("Unknown", "Hitchhiker's Guide to the Universe", "850", false);
 myLibrary.remove("1984");
 myLibrary.update("Steve Jobs", "read", true);
-console.table(myLibrary.books);
+
 
 // DOM manipulation
-// 1. Create function to add new book to DOM
- 
-// 2. Create functions edit a specific book in the DOM
+function addToDOM(title, author, pages, read) {
+    let cardClone = cardTemplate.content.cloneNode(true);
+    let titleText = cardClone.querySelector(".title");
+    let authorText = cardClone.querySelector(".author");
+    let pagesText = cardClone.querySelector(".pages");
+    let readText = cardClone.querySelector(".read");
 
-// 3. Create function to add all books to DOM
+    titleText.textContent = title;
+    authorText.textContent = author;
+    pagesText.textContent = pages;
+    readText.textContent = read;
+
+    article.appendChild(cardClone);
+}
+function removeAllFromDOM() {
+    while (article.hasChildNodes()) {
+        article.removeChild(article.lastChild);
+    }
+}
+
+
+// Events
+addButton.addEventListener("click", () => {
+    myLibrary.add("1984", "George Orwell", 650, true);
+    addToDOM("1984", "George Orwell", "650", "true");
+});
+
+removeAllButton.addEventListener("click", () => {
+    myLibrary.removeAll();
+    removeAllFromDOM();
+});
